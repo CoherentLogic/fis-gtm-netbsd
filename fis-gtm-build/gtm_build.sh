5ad7498d8a4b96403cb0d305ba0b4ef6fe438d16
John Willis <jpw@coherent-logic.com>
2015-05-16 00:47:51 +0000

    First commit

---
+++ fis-gtm-build/gtm_build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+export LC_CTYPE=$(locale -a | gawk 'BEGIN{IGNORECASE=1}/en_us.utf-*8/{print;exit}')
+export LANG=C LC_ALL= LC_COLLATE=C
+
+echo "Build Environment"
+echo "-----------------"
+echo "LC_CTYPE=${LC_CTYPE} LANG=${LANG} LC_ALL=${LC_ALL} LC_COLLATE=${LC_COLLATE}"
+echo ""
+echo "cmake -D CMAKE_INSTALL_PREFIX:PATH=${PWD}/package ../"
+echo "* * * * * * * *"
+
+cmake -D CMAKE_INSTALL_PREFIX:PATH=${PWD}/package ../
+echo "make; sudo make install"
+echo "* * * * * * * *"
+
+make; sudo make install
+
+
